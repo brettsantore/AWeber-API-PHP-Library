82c8257dfde2264039d48ce054e6f8245a0d6bdb
Brett Santore <brett.santore@gmail.com>
2017-05-12 12:24:21 -0400

    move service provider into psr4 namespace

--- aweber_api/aweber.php
+++ aweber_api/aweber.php
@@ -8,7 +8,7 @@
  * @package
  * @version $id$
  */
-class AWeberServiceProvider implements OAuthServiceProvider {
+class AWeberServiceProvider implements \AWeber\OAuth\ServiceProvider {
 
     /**
      * @var String Location for API calls
@@ -1173,37 +1173,7 @@ class AWeberResponseError extends AWeberException {
 
 
 
-/**
- * OAuthServiceProvider
- *
- * Represents the service provider in the OAuth authentication model.
- * The class that implements the service provider will contain the
- * specific knowledge about the API we are interfacing with, and
- * provide useful methods for interfacing with its API.
- *
- * For example, an OAuthServiceProvider would know the URLs necessary
- * to perform specific actions, the type of data that the API calls
- * would return, and would be responsible for manipulating the results
- * into a useful manner.
- *
- * It should be noted that the methods enforced by the OAuthServiceProvider
- * interface are made so that it can interact with our OAuthApplication
- * cleanly, rather than from a general use perspective, though some
- * methods for those purposes do exists (such as getUserData).
- *
- * @package
- * @version $id$
- */
-interface OAuthServiceProvider {
 
-    public function getAccessTokenUrl();
-    public function getAuthorizeUrl();
-    public function getRequestTokenUrl();
-    public function getAuthTokenFromUrl();
-    public function getBaseUri();
-    public function getUserData();
-
-}
 
 /**
  * OAuthApplication
@@ -1258,7 +1228,7 @@ class OAuthApplication implements \AWeber\OAuth\Adapter {
      */
     public function __construct($parentApp = false) {
         if ($parentApp) {
-            if (!is_a($parentApp, 'OAuthServiceProvider')) {
+            if (!is_a($parentApp, \AWeber\OAuth\ServiceProvider::class)) {
                 throw new Exception('Parent App must be a valid OAuthServiceProvider!');
             }
             $this->app = $parentApp;
