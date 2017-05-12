0cfcab589808414fd07b336eb67173fed9b71855
Brett Santore <brett.santore@gmail.com>
2017-05-12 12:17:23 -0400

    move CurlResponse into psr-4 namespace

--- aweber_api/aweber.php
+++ aweber_api/aweber.php
@@ -1043,53 +1043,6 @@ class AWeberResponse extends AWeberAPIBase {
  * @version $id$
  */
 
-
-
-
-
-
-class CurlResponse
-{
-    public $body = '';
-    public $headers = array();
-
-    public function __construct($response)
-    {
-        # Extract headers from response
-        $pattern = '#HTTP/\d\.\d.*?$.*?\r\n\r\n#ims';
-        preg_match_all($pattern, $response, $matches);
-        $headers = explode("\r\n", str_replace("\r\n\r\n", '', array_pop($matches[0])));
-
-        # Extract the version and status from the first header
-        $version_and_status = array_shift($headers);
-        preg_match('#HTTP/(\d\.\d)\s(\d\d\d)\s(.*)#', $version_and_status, $matches);
-        $this->headers['Http-Version'] = $matches[1];
-        $this->headers['Status-Code'] = $matches[2];
-        $this->headers['Status'] = $matches[2].' '.$matches[3];
-
-        # Convert headers into an associative array
-        foreach ($headers as $header) {
-            preg_match('#(.*?)\:\s(.*)#', $header, $matches);
-            $this->headers[$matches[1]] = $matches[2];
-        }
-
-        # Remove the headers from the response body
-        $this->body = preg_replace($pattern, '', $response);
-    }
-
-    public function __toString()
-    {
-        return $this->body;
-    }
-
-    public function headers(){
-        return $this->headers;
-    }
-}
-
-
-
-
 class AWeberException extends Exception { }
 
 /**
@@ -1804,7 +1757,7 @@ class OAuthApplication implements AWeberOAuthAdapter {
         $this->curl->setopt($handle, CURLOPT_TIMEOUT, 90);
         $resp = $this->curl->execute($handle);
         if ($resp) {
-            return new CurlResponse($resp);
+            return new \AWeber\Curl\Response($resp);
         }
         $this->error = $this->curl->errno($handle) . ' - ' .
             $this->curl->error($handle);
